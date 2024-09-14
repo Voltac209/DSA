1f6358382dead6c16861332fc5f1a6d502702e27
Raghav Sood <raghavsood3678@gmail.com>
2024-09-14 11:50:10 +0530

    Revise the Code

---
+++ Binary Search/MinimumRA.java
@@ -0,0 +1,32 @@
+package DSA;
+
+public class MinimumRA {
+    public int findMin(int[] nums) {
+        return halves(nums);
+    }
+    public int halves(int[] nums){
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        int ans=nums[0];
+        while (low<=high){
+            int mid=(low+high)/2;
+            if (nums[mid]>nums[high]){
+                if (nums[mid]<ans){
+                    ans=nums[mid];
+
+                }
+                low=mid+1;
+
+            }
+            else {
+                if (nums[mid]<ans){
+                    ans=nums[mid];
+
+                }
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+}
