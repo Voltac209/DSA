fa94bf6770ed18869298ecc671ddb3162b12a046
Raghav Sood <raghavsood3678@gmail.com>
2024-09-27 23:00:49 +0530

    Logic Revision

---
+++ Linked List/Basic/DeleteKthElement.java
@@ -0,0 +1,15 @@
+package DSA;
+
+public class DeleteKthElement {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+    class Solution {
+        public void deleteNode(ListNode node) {
+            node.val=node.next.val;
+            node.next=node.next.next;
+        }
+    }
+}
