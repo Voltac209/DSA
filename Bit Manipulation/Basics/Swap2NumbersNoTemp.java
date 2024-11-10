55694668fc91b6f362e7b16836d5c189e3b8aa82
Voltac209 <raghavsood3678@gmail.com>
2024-11-10 17:31:08 +0530

    Key Idea XOR operator

---
+++ Bit Manipulation/Basics/Swap2NumbersNoTemp.java
@@ -0,0 +1,17 @@
+package DSA;
+import java.util.*;
+public class Swap2NumbersNoTemp {
+    class Solution{
+        static List<Integer> get(int a,int b)
+        {
+            // code here
+            a=a^b;
+            b=a^b;
+            a=a^b;
+            List<Integer> res=new ArrayList<>();
+            res.add(a);
+            res.add(b);
+            return res;
+        }
+    }
+}
