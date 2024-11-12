17c64ecbb9c2b097b7362d5fe4280c2c7ccd7e32
Voltac209 <raghavsood3678@gmail.com>
2024-11-12 18:17:06 +0530

    Logic Revision Needed

---
+++ Bit Manipulation/Interview Problems/MinimumNoOfBitFlips.java
@@ -0,0 +1,15 @@
+package DSA;
+
+public class MinimumNoOfBitFlips {
+    class Solution {
+        public int minBitFlips(int start, int goal) {
+            int ans = start ^ goal;
+            int cnt = 0;
+            while (ans > 0) {
+                cnt += ans & 1;
+                ans >>= 1;
+            }
+            return cnt;
+        }
+    }
+}
