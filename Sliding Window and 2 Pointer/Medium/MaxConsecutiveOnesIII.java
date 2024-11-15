63717d6ea5410af4e8d3bf3588bfd133f8a89177
Voltac209 <raghavsood3678@gmail.com>
2024-11-15 21:15:10 +0530

    Key Idea is maximum k number of 0 in a valid window

---
+++ Sliding Window and 2 Pointer/Medium/MaxConsecutiveOnesIII.java
@@ -0,0 +1,26 @@
+package DSA;
+
+public class MaxConsecutiveOnesIII {
+    class Solution {
+        public int longestOnes(int[] nums, int k) {
+            int left=0;
+            int right=0;
+            int count=0;
+            int res=0;
+            while (right<nums.length){
+                if (nums[right]==0){
+                    count++;
+                }
+                while (count>k){
+                    if (nums[left]==0){
+                        count--;
+                    }
+                    left++;
+                }
+                res=Math.max(res,right-left+1);
+                right++;
+            }
+            return res;
+        }
+    }
+}
